e78ea72b2943254dd7a92c3923a61373075f45aa
viniciusdevjesus <viniciusjesusfilho07@gmail.com>
2024-05-23 11:17:18 -0300

    feat: import components on App

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,16 @@ import {
 } from "react-router-dom";
 import Header from './components/header';
 import Footer from './components/footer';
+import ProductCards from './components/ProductCards';
 
 function App() {
 
   return (
     <>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    
     <Router>
       <Header />
-
+      <ProductCards />
       <Footer />
     </Router>
     </>
